e3edf6678db4be55231380a87b2914745ff4499c
Hemza-Assebab <assebabhamza03@gmail.com>
2025-09-06 00:49:43 +0100

    docs: re-orginized documentation

--- 02-basic-syntax/main.c
+++ 02-basic-syntax/main.c
@@ -1,33 +1,27 @@
-/**
- * File: 02-basic-syntax/main.c
- * Topic: Basic Syntax
- * Author: Hemza Assebab
- * Date: 09/05/2025
- * Description:
- *    This file explains the basic syntax of a C program,
- *    including main function and simple printing
- */
+/* Basic Syntax */
 
-/**
- * #include is a Pre-processor directive that includes the stdio header file.
- * It lets us work with functions like [printf(), scanf()...].
- * Header files add functionality to C programs.
- */
+/* What is stdio.h ? */
+/*
+    - #include is a Pre-processor directive that includes the stdio header file.
+    - It lets us work with functions like [printf(), scanf()...].
+    - Header files add functionality to C programs.
+*/
 #include <stdio.h>
 
-/**
- * @brief This function is the entry point of the program.
- * @return int Returns 0 if the program runs successfully, and 1 if not.
- */
+/* main() */
+/*
+    -This function is the entry point of the program.
+    -It returns 0 if the program runs successfully, and 1 if not.
+*/
 int main() {
 
-    /**
-     * NOTES: 
-     * - Every statement in C ends with a semicolon ";" .
-     * - A computer program is a list of instructions to be executed by a computer.
-     * - In a programming language these instructions are called statements
-     * - If your program has many statements they will be executed one bye one in the same order as they're written
-     */
+    /* Notes */
+    /*
+        - Every statement in C ends with a semicolon ";" .
+        - A computer program is a list of instructions to be executed by a computer.
+        - In a programming language these instructions are called statements
+        - If your program has many statements they will be executed one bye one in the same order as they're written
+    */
 
     printf("Hello there\n"); // This statement instructs the compiler to print "Hello there"
     printf("I am Hemza\n");
